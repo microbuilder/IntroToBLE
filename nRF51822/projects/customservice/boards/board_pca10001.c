498d4a150a98b2756eb95ad965650226448d6414
hathach <thach@tinyusb.org>
2014-04-29 12:46:04 +0700

    clean up board, move helper to end of the file for easy code reading

--- nRF51822/projects/customservice/boards/board_pca10001.c
+++ nRF51822/projects/customservice/boards/board_pca10001.c
@@ -11,25 +11,10 @@ const static uint8_t led_gpio [BOARD_LED_NUM] = { BOARD_LED_PIN_ARRAY };
 const static uint8_t button_gpio [BOARD_BUTTON_NUM] = { BOARD_BUTTON_PIN_ARRAY };
 
 
-/**************************************************************************/
-/*!
-    @brief  Helper function that enables or disables the LED pins
-*/
-/**************************************************************************/
 static inline uint32_t led_mask_to_gpio(uint8_t led_mask) ATTR_ALWAYS_INLINE ATTR_PURE;
-static inline uint32_t led_mask_to_gpio(uint8_t led_mask)
-{
-  uint32_t result = 0;
-  for(uint32_t i=0; i<BOARD_LED_NUM; i++)
-  {
-    if ( BIT_TEST(led_mask,i) )
-    {
-      result = BIT_SET(result, led_gpio[i]);
-    }
-  }
+static inline uint8_t button_gpio_to_number(uint8_t pin_no) ATTR_ALWAYS_INLINE ATTR_PURE;
+static inline uint32_t get_baudrate(uint32_t decimal_value) ATTR_ALWAYS_INLINE ATTR_CONST;
 
-  return result;
-}
 
 /**************************************************************************/
 /*!
@@ -44,26 +29,10 @@ static inline uint32_t led_mask_to_gpio(uint8_t led_mask)
 /**************************************************************************/
 void boardLED(uint8_t mask_on, uint8_t mask_off)
 {
-  // TODO BOARD_LED_ON == 0 not implement
   NRF_GPIO->OUTCLR = led_mask_to_gpio(mask_off);
   NRF_GPIO->OUTSET = led_mask_to_gpio(mask_on ); // mask_on take over priority
 }
 
-/**************************************************************************/
-/*!
-    @brief  Correlates a pin and button numbers
-*/
-/**************************************************************************/
-static inline uint8_t button_gpio_to_number(uint8_t pin_no) ATTR_ALWAYS_INLINE ATTR_PURE;
-static inline uint8_t button_gpio_to_number(uint8_t pin_no)
-{
-  for(uint8_t i=0; i<BOARD_BUTTON_NUM; i++)
-  {
-    if ( button_gpio[i] == pin_no ) return i;
-  }
-
-  return 0;
-}
 
 /**************************************************************************/
 /*!
@@ -103,36 +72,6 @@ void board_uart_event_handler(app_uart_evt_t* p_event)
          p_event->evt_type != APP_UART_COMMUNICATION_ERROR , (void) 0);
 }
 
-/**************************************************************************/
-/*!
-    @brief  Helper function to convert baud rates to a proper enum value
-*/
-/**************************************************************************/
-static inline uint32_t get_baudrate(uint32_t decimal_value) ATTR_ALWAYS_INLINE ATTR_CONST;
-static inline uint32_t get_baudrate(uint32_t decimal_value)
-{
-  switch(decimal_value)
-  {
-    case 1200    : return UART_BAUDRATE_BAUDRATE_Baud1200   ;
-    case 2400    : return UART_BAUDRATE_BAUDRATE_Baud2400   ;
-    case 4800    : return UART_BAUDRATE_BAUDRATE_Baud4800   ;
-    case 9600    : return UART_BAUDRATE_BAUDRATE_Baud9600   ;
-    case 14400   : return UART_BAUDRATE_BAUDRATE_Baud14400  ;
-    case 19200   : return UART_BAUDRATE_BAUDRATE_Baud19200  ;
-    case 28800   : return UART_BAUDRATE_BAUDRATE_Baud28800  ;
-    case 38400   : return UART_BAUDRATE_BAUDRATE_Baud38400  ;
-    case 57600   : return UART_BAUDRATE_BAUDRATE_Baud57600  ;
-    case 76800   : return UART_BAUDRATE_BAUDRATE_Baud76800  ;
-    case 115200  : return UART_BAUDRATE_BAUDRATE_Baud115200 ;
-    case 230400  : return UART_BAUDRATE_BAUDRATE_Baud230400 ;
-    case 250000  : return UART_BAUDRATE_BAUDRATE_Baud250000 ;
-    case 460800  : return UART_BAUDRATE_BAUDRATE_Baud460800 ;
-    case 921600  : return UART_BAUDRATE_BAUDRATE_Baud921600 ;
-    case 1000000 : return UART_BAUDRATE_BAUDRATE_Baud1M     ;
-  }
-
-  ASSERT(false, 0);
-}
 
 /**************************************************************************/
 /*!
@@ -187,6 +126,75 @@ void boardInit(void)
   /* Initialise the UART FIFO */
   APP_UART_FIFO_INIT( &para_uart, CFG_UART_BUFSIZE, CFG_UART_BUFSIZE, board_uart_event_handler, APP_IRQ_PRIORITY_LOW, error);
   ASSERT_STATUS_RET_VOID( (error_t) error );
+}
+
+/* ---------------------------------------------------------------------- */
+/* HELPER FUNCTIONS                                                       */
+/* ---------------------------------------------------------------------- */
+
+/**************************************************************************/
+/*!
+    @brief  Helper function to convert baud rates to a proper enum value
+*/
+/**************************************************************************/
+static inline uint32_t get_baudrate(uint32_t decimal_value)
+{
+  switch(decimal_value)
+  {
+    case 1200    : return UART_BAUDRATE_BAUDRATE_Baud1200   ;
+    case 2400    : return UART_BAUDRATE_BAUDRATE_Baud2400   ;
+    case 4800    : return UART_BAUDRATE_BAUDRATE_Baud4800   ;
+    case 9600    : return UART_BAUDRATE_BAUDRATE_Baud9600   ;
+    case 14400   : return UART_BAUDRATE_BAUDRATE_Baud14400  ;
+    case 19200   : return UART_BAUDRATE_BAUDRATE_Baud19200  ;
+    case 28800   : return UART_BAUDRATE_BAUDRATE_Baud28800  ;
+    case 38400   : return UART_BAUDRATE_BAUDRATE_Baud38400  ;
+    case 57600   : return UART_BAUDRATE_BAUDRATE_Baud57600  ;
+    case 76800   : return UART_BAUDRATE_BAUDRATE_Baud76800  ;
+    case 115200  : return UART_BAUDRATE_BAUDRATE_Baud115200 ;
+    case 230400  : return UART_BAUDRATE_BAUDRATE_Baud230400 ;
+    case 250000  : return UART_BAUDRATE_BAUDRATE_Baud250000 ;
+    case 460800  : return UART_BAUDRATE_BAUDRATE_Baud460800 ;
+    case 921600  : return UART_BAUDRATE_BAUDRATE_Baud921600 ;
+    case 1000000 : return UART_BAUDRATE_BAUDRATE_Baud1M     ;
+  }
+
+  ASSERT(false, 0);
+  return 0;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Correlates a pin and button numbers
+*/
+/**************************************************************************/
+static inline uint8_t button_gpio_to_number(uint8_t pin_no)
+{
+  for(uint8_t i=0; i<BOARD_BUTTON_NUM; i++)
+  {
+    if ( button_gpio[i] == pin_no ) return i;
+  }
+
+  return 0;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Helper function that enables or disables the LED pins
+*/
+/**************************************************************************/
+static inline uint32_t led_mask_to_gpio(uint8_t led_mask)
+{
+  uint32_t result = 0;
+  for(uint32_t i=0; i<BOARD_LED_NUM; i++)
+  {
+    if ( BIT_TEST(led_mask,i) )
+    {
+      result = BIT_SET(result, led_gpio[i]);
+    }
+  }
+
+  return result;
 }
 
 #endif
